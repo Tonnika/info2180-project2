e5bb1e7ef99d991c62737895ccea2955c1d3aff9
Tonnika <shantiwilliams11@gmail.com>
2018-11-06 23:32:03 -0800

    changes

--- fifteen.js
+++ fifteen.js
@@ -1,12 +1,27 @@
 "use strict"; 
-window.onload = function (){
+window.onload = function(){
 	var body = document.getElementsByTagName("body");
 	console.log(body);
 	
-	var puzzleArea = $("#puzzlearea");
+	var puzzleArea = document.getElementById("puzzlearea")
 	var puzzlePiece = puzzleArea.children(); 
-	
+	var count =0;
+	var hoz	=0;
+	var ver =0;
 	for(var i=0; i<puzzlePiece.length; i++){
-		$(puzzlePiece[i].classList.add("puzzlepiece");
-	}
+		puzzlePiece[i].classList.add("puzzlepiece");
+		puzzlePiece[i].style.top = hor + "px"; 
+		puzzlePiece[i].style.left = ver + "px";
+		puzzlePiece[i].style.backgroundPosition= '-' + puzzlePiece[i].style.left + ' ' + '-' + puzzlePiece[i].style.top; 
+		hor += 100; 
+		count +=1;
+		
+		if(count%4 ==0){
+			ver +=100;
+			hor=0;
+		}
+		
+		
+	};
+	this.style.backgroundImage="url('background.jpg');
 };
